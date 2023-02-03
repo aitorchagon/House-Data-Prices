9028815357a4fb6d5c6fa4aaf3136c47e0becdb6
Aitor <aitorchagon@gmail.com>
2023-02-03 13:59:52 +0100

    Started to encode nominal categorical features, preparing OneHotEncoding

--- house_data_prices.py
+++ house_data_prices.py
@@ -8,7 +8,8 @@ Created on Thu Feb  2 16:20:50 2023
 
 import pandas as pd
 import miceforest as mf
-
+from sklearn.preprocessing import MinMaxScaler, LabelEncoder, OneHotEncoder
+import numpy as np
 
 train = pd.read_csv('test.csv')
 test = pd.read_csv('train.csv')
@@ -23,7 +24,7 @@ del house['Id']
 #this way, we will be ready to study statistical properties properly.
 
 #Street,CentralAir, are binary valued, we need to take that into account.
-nominal_categorical = ['MSubClass', 'MSZoning', 'Street', 'Alley', 'LotConfig',
+nominal_categorical = ['MSSubClass', 'MSZoning', 'Street', 'Alley', 'LotConfig',
                        'Neighborhood', 'Condition1', 'Condition2', 'BldgType',
                        'HouseStyle', 'RoofStyle', 'RoofMatl', 'Exterior1st',
                        'Exterior2nd', 'MasVnrType', 'Foundation', 'Heating',
@@ -54,32 +55,44 @@ null_values = house.isnull().sum()
     -PoolQC: 2909/2919 -> 99.66%
     -Fence: 2348/2919 -> 80.43%
     -MiscFeature: 2814/2919 -> 96.4%
+    -FireplaceQu: 1420/2919 -> 48.65%
 As a result, we will proceed to delete those columns right away
 We will deal with the rest of them using MICE forest algorithm'''
 
-columns_to_delete = ['Alley', 'PoolQC', 'Fence', 'MiscFeature']
+columns_to_delete = ['Alley', 'PoolQC', 'Fence', 'MiscFeature', 'FireplaceQu']
 house = house.drop(columns_to_delete, axis = 1)
 nominal_categorical.remove('Alley')
 nominal_categorical.remove('MiscFeature')
 ordinal_categorical.remove('PoolQC')
 ordinal_categorical.remove('Fence')
+ordinal_categorical.remove('FireplaceQu')
 
-
-#Now we will deal with the rest of the imputations, as they are all under 50% of missing values.
-#hay que pensar cómo hacer esto teniendo en cuenta que no queremos codificar las categorías
-#(después va a ser difícil hacer onehotencoding o lo que sea en las nominales)
-mice = mf.ImputationKernel(
-  house,
-  save_all_iterations=True,
-  random_state=1999
-)
-
-mice.mice(2)
-
-house = mice.complete_data()
 null_values_ = house.isnull().sum()
-#let's see whether we have cleaned our data.
 
-#Let's see a statistical description of our data before turning to the plots.
-description_data = house.describe()
+#Now we will proceed to codify the rest of variables; we will codify, temporarily, 
+#nan values as a number; then, we will change this number for a np.nan and execute our
+#mice forest model. In the case of nominal feature, we will not impute a value but remove them.
+#As the amount of them is very low
+
+ohe = OneHotEncoder(categories= 'auto', sparse_output = False)
+ohe_df = pd.DataFrame()
+#MSubClass has two categories quite similar 'Split or multi-level" and "Split"
+#The same occurs with 1 and 1/2 finished and unfinished, 2 and 2 1/2
+#let's merge them
+house['MSSubClass'] = house['MSSubClass'].replace(85, 80)
+house['MSSubClass'] = house['MSSubClass'].replace(45, 50)
+house['MSSubClass'] = house['MSSubClass'].replace(150, 120)
+house['MSSubClass'] = house['MSSubClass'].replace(40, 20)
+house['MSSubClass'] = house['MSSubClass'].replace(75, 60)
+
+df_number_categories = pd.DataFrame(columns = nominal_categorical)   
+for elem in nominal_categorical:
+    number = house[elem].value_counts(sort = False)
+    df_number_categories = pd.concat([number, df_number_categories], axis = 1, join = 'outer')
+    df_number_categories = df_number_categories.replace(np.nan, 0)
+
+  
+
+
+
 
